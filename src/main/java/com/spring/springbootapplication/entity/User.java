16ddad7ad5861f980797766dff63f81148ab6daf
yuto0730 <yuto.suzuki0730@gmail.com>
2026-03-21 13:56:40 +0900

    feat:ユーザー情報を管理するEntity(User.java)を作成

---
+++ src/main/java/com/spring/springbootapplication/entity/User.java
@@ -0,0 +1,27 @@
+package com.spring.springbootapplication.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
