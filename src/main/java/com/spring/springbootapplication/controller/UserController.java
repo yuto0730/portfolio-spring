036cb586e1ce715f30a6fa41eb41035274d17139
yuto0730 <yuto.suzuki0730@gmail.com>
2026-03-21 16:00:53 +0900

    登録ボタンを押したらUserserviceを呼び出す

--- src/main/java/com/spring/springbootapplication/controller/UserController.java
+++ src/main/java/com/spring/springbootapplication/controller/UserController.java
@@ -1,14 +1,31 @@
 package com.spring.springbootapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
+import com.spring.springbootapplication.entity.User;
+import com.spring.springbootapplication.service.UserService;
 
 @Controller
 public class UserController {
-    
-    //ブラウザで /signupにアクセスしたら登録画面を返す
-    @GetMapping
-    public String ahowSignupForm() {
-        return "signup"; //templates/signup.htmlを表示
+
+    @Autowired
+    private UserService userService; 
+
+    //【GET】ブラウザで /signup にアクセスした時に、登録画面を表示する
+    @GetMapping("/signup")
+    public String showSignupForm() {
+        return "signup";
+    }
+
+    //【POST】登録ボタンが押された時に、入力されたデータを受け取って保存する
+    @PostMapping("/signup")
+    public String register(User user) {
+        // 画面から届いた user 情報を、判定ルール(Service)に渡して保存してもらう
+        userService.registerUser(user);
+        
+        // 保存が終わったら、TOP画面（今はまだないので、とりあえずルート / ）へリダイレクト
+        return "redirect:/";
     }
 }
