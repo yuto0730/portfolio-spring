21e93f64a206dfe598b851bd9ddc917464d99947
yuto0730 <yuto.suzuki0730@gmail.com>
2026-04-13 03:45:38 +0900

    feat: 自己紹介編集機能とバリデーション修正

--- src/main/java/com/spring/springbootapplication/form/ProfileEditForm.java
+++ src/main/java/com/spring/springbootapplication/form/ProfileEditForm.java
@@ -8,7 +8,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProfileEditForm {
 
     // 要件：50文字以上200文字以下
-    // 初期値を空文字("")にしておくことで、入力がない場合は「空っぽ」としてエラーになるようにする
     @Size(min = 50, max = 200, message = "自己紹介は50文字以上200文字以下で入力してください")
     private String selfIntroduction = "";
 
