b45108c74e3ef515c8a6353ebe61dce7d7054da9
yuto0730 <yuto.suzuki0730@gmail.com>
2026-04-19 00:42:01 +0900

    feat: LearningDataエンティティの作成

---
+++ src/main/java/com/spring/springbootapplication/entity/LearningData.java
@@ -0,0 +1,45 @@
+package com.spring.springbootapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "learning_data")
+@Data
+public class LearningData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id; // ID
+
+    // ユーザーID
+    @Column(name = "user_id")
+    private Integer userId;
+
+    // カテゴリーID
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    // 学習項目名
+    @Column(name = "name", length = 50)
+    private String name;
+
+    // 学習時間
+    @Column(name = "study_time")
+    private Integer studyTime;
+
+    // 学習月
+    @Column(name = "study_month")
+    private LocalDate studyMonth;
+
+    // 作成日時
+    @Column(name = "created_id")
+    private LocalDateTime createdId;
+
+    // 更新日時
+    @Column(name = "updated_id")
+    private LocalDateTime updatedId;
+}
