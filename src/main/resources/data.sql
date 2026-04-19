f744bfc3c3af54aa68fb34b133f6f60bff0a8d32
yuto0730 <yuto.suzuki0730@gmail.com>
2026-04-19 20:45:39 +0900

    chore: ローカル開発用の初期データ(categories)を追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories (name, created_at, updated_at) VALUES ('バックエンド', NOW(), NOW());
+INSERT INTO categories (name, created_at, updated_at) VALUES ('フロントエンド', NOW(), NOW());
+INSERT INTO categories (name, created_at, updated_at) VALUES ('インフラ', NOW(), NOW());
