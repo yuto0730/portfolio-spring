ff228c062d10ac0218d09285180c8677853456a8
yuto0730 <yuto.suzuki0730@gmail.com>
2026-04-26 20:33:00 +0900

    微修正

--- src/main/java/com/spring/springbootapplication/service/LearningDataService.java
+++ src/main/java/com/spring/springbootapplication/service/LearningDataService.java
@@ -14,20 +14,24 @@ public class LearningDataService {
     @Autowired
     private LearningDataRepository learningDataRepository;
 
-    //学習時間を更新するメソッド
+    // 学習時間を更新するメソッド
     public void updateStudyTime(Integer id, Integer studyTime) {
         
-        //受け取ったIDを使って、データベースから該当の学習データを探し出す
+        if (id == null) {
+            return;
+        }
+
+        // 受け取ったIDを使って、データベースから該当の学習データを探し出す
         LearningData learningData = learningDataRepository.findById(id).orElse(null);
         
-        //データが無事に見つかった場合のみ、上書き処理を行う
+        // データが無事に見つかった場合のみ、上書き処理を行う
         if (learningData != null) {
-            //学習時間を新しい値にセットする
+            // 学習時間を新しい値にセットする
             learningData.setStudyTime(studyTime);
-            //更新日時も「今」の時間に上書きしておく
+            // 更新日時も「今」の時間に上書きしておく
             learningData.setUpdatedAt(LocalDateTime.now());
             
-            //変更した内容をデータベースに保存（UPDATE）する
+            // 変更した内容をデータベースに保存（UPDATE）する
             learningDataRepository.save(learningData);
         }
     }
